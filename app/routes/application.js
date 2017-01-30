446d1059ceb5504edebc662bc8de1dd23e0d086b
Brian Gonzalez <me@briangonzalez.org>
2017-01-29 16:56:23 -0800

    in memory version

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,7 +3,15 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
   model() {
-    return this.store.findRecord('user', 'me');
+    const user = this.store.createRecord('user', { id: 'me', name: 'Jacob Kelley' });
+
+    user.get('paymentMethods').pushObjects([
+      this.store.createRecord('payment-method-paypal', { id: '1', linkedEmail: 'me@briangonzalez.org' }),
+      this.store.createRecord('payment-method-apple-pay', { id: '2', fingerprintHash: 'AE7676JHG' }),
+      this.store.createRecord('payment-method-cc', { id: '3', last4: '1234' }),
+    ]);
+
+    return user;
   }
 
 });
